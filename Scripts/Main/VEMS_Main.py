979f1fe51d75e6ab4c9cc11df533567882aed647
justind-dev <39993170+justind-dev@users.noreply.github.com>
2020-01-03 21:27:54 -0500

    Create VEMS_Main.py
    Added VEMS_Main.py which will contain the command line interface for running all the tools included in VEMS.

---
+++ Scripts/Main/VEMS_Main.py
@@ -0,0 +1,5 @@
+#THIS IS THE MAIN MENU PROGRAM FOR THE VEMS SOFTWARE. YOU USE THIS TO RUN THE REST OF THE SCRIPTS FROM A GUI INTERFACE.
+import os
+import time
+print("Welcome to the VEMS Software. \n It is currently in beta mode. Enjoy.")
+time.sleep(5)
