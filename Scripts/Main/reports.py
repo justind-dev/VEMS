d1448f8ff6119d92d9ba5f649db9f1bb50a65c5e
JustinD-Dev <39993170+justind-dev@users.noreply.github.com>
2021-08-12 19:05:05 -0400

    Updated different things see desc
    Updated reports.py to utilize keyring for connection information in case you want to schedule the reports script to be ran. This is the secure way of doing so. Modified toolbelt to reflect this, as well as making some basic changes for storing certificate expiration information. Added check for hosts whose certificate information could not be retrieved.

--- Scripts/Main/reports.py
+++ Scripts/Main/reports.py
@@ -1,19 +1,24 @@
 """
-Basic Python scripts for utlizing pyvmomi with the vsphere enviroment.
+Customizable reports for vSphere enviornment
 
 VMWare
 Environment
 Management
 Scripts'
 
+You will need to store credentials for smtp and the vsphere connection 
+using keyring module first. If you use active directory do so in this
+format: domain\\samaccountname
+credential service name = vems_vsphere
+username = vsphere username
+password = vsphere password
 """
-
+import smtplib, ssl, keyring
 from toolbelt import Vcenter
 
 def main():
-    connection = Vcenter(host="",user="",pwd="",use_ssl=False)
+    connection = Vcenter(host="vcenter",user="example@vsphere.local",use_ssl=False)
     connection.get_hosts()
-    
     connection.get_certificate_expirations()
 
     for host in connection.certificates_expiring_in_days(30):
--- Scripts/Main/toolbelt.py
+++ Scripts/Main/toolbelt.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
+import keyring
 from pyVim.connect import SmartConnectNoSSL, Disconnect
 from pyVmomi import vim, vmodl
 import datetime
 import pytz
 
 class Vcenter:
-    def __init__(self,host,user,pwd,port=443,use_ssl=True):
+    def __init__(self,host,user,port=443,use_ssl=True):
         self.host = host
         self.user = user
-        self.pwd = pwd
+        self.pwd = keyring.get_password("vems_vsphere",self.user)
         self.port = port
         self.use_ssl = use_ssl
         self.si = self.connect()
         self.content = ''
         self.allhosts = {}
-       
+        self.certficate_expirations = {}
+
     def connect(self):
         if self.use_ssl:
             raise Exception("SSL not yet implemented")
@@ -27,7 +29,7 @@ class Vcenter:
             return 0
 
     def get_hosts(self):
-        self.__allhosts =  self.si.content.viewManager.CreateContainerView(self.si.content.rootFolder,
+        self.allhosts =  self.si.content.viewManager.CreateContainerView(self.si.content.rootFolder,
                                                             [vim.HostSystem],
                                                             True).view
     def print_hostnames(self):
@@ -35,20 +37,20 @@ class Vcenter:
             print(host.name)
 
     def get_certificate_expirations(self):
-        expirations = {}
         for host in self.allhosts:
             try:
-                expirations[host.name] = host.configManager.certificateManager.certificateInfo.notAfter
+                self.certficate_expirations[host.name] = host.configManager.certificateManager.certificateInfo.notAfter
             except:
-                datetime.datetime(1988, 12, 30)
+                self.certficate_expirations[host.name] = "Could not retrieve"
                 continue
-        return expirations
+        
     
     def certificates_expiring_in_days(self, number_of_days):
         expirations = []
         expiration_datetime = datetime.datetime.now() + datetime.timedelta(days=number_of_days)
-        for k, v in self.certificate_expirations().items():
-            if v <= pytz.timezone('US/Eastern').localize(expiration_datetime):
-                expirations.append(k)
+        for k, v in self.certficate_expirations.items():
+            if not k == "Could not retrieve":
+                if v <= pytz.timezone('US/Eastern').localize(expiration_datetime):
+                    expirations.append(k)
         return expirations
     
